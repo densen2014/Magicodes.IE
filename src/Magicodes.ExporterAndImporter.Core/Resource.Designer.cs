6981558d60fcb8f02c9c067786ed5da2632a3c2d
Alex chow <zhouchuanglin@gmail.com>
2024-01-13 18:11:44 +0100

    test2

--- src/Magicodes.ExporterAndImporter.Core/Resource.Designer.cs
+++ src/Magicodes.ExporterAndImporter.Core/Resource.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace Magicodes.IE.Core {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace Magicodes.IE.Core {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Magicodes.IE.Core.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Magicodes.IE.Core.Resource", typeof(Resource).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
